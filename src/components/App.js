f61738e14de0aaa83e65900dc22bd11d391a4308
Thomas Nielse <skt.thomas@gmail.com>
2020-02-07 13:38:37 +0100

    V1.0 Added player, added default search term, added styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar'
 import Youtube from '../api/youtube'
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
 
@@ -15,18 +16,31 @@ class App extends React.Component{
             }
         });
         
-        this.setState({videos: response.data.items})
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0] })
     };
 
     onVideoSelect = (video) => {
-        console.log('From the app! ',video)
+        this.setState({selectedVideo: video})
+    }
+
+    componentDidMount() {
+        this.onTermSubmit('explosions')
     }
 
     render() {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>  
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 const VideoItem = ({video, onVideoSelect}) =>{ //onVideoSelect has been passed down from App, to Videolist, to here. Any onClick event is passed back up to App
 return (
 <div onClick={() => onVideoSelect(video)} className="video-item item">
-    <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+    <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
     <div className="content">
         <div className="header">
             {video.snippet.title}
